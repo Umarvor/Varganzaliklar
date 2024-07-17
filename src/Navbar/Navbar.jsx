61a764372e29bd5f7ad60aa0f7b9b5b91f70f872
Shohinur <shohinurshahriyorov@gmail.com>
2024-07-17 10:10:54 +0500

    sffgfjfb

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import Menu from '../img/burger-menu.png'
-import { useState,useRef } from 'react';
+import { useState, useRef } from 'react';
 import axios from 'axios';
 import VC from '../img/Снимок_экрана_2024-07-15_130625-removebg-preview.png'
 function Navbar() {
-    const [showCommentBox, setShowCommentBox] = useState(false);
+  const [showCommentBox, setShowCommentBox] = useState(false);
+  const [showCommentBoxx, setShowCommentBoxx] = useState(false);
   const commentRef = useRef(null);
 
   const handleToggleCommentBox = () => {
     setShowCommentBox(!showCommentBox);
   };
-
+  const handleToggleCommentBoxx = () => {
+    setShowCommentBoxx(!showCommentBoxx);
+  };
   const handleSendComment = async () => {
     const comment = commentRef.current.value;
     if (comment) {
@@ -37,48 +40,77 @@ function Navbar() {
     }
   };
 
- 
-    return (
-        <>
-            <div className='Navbar'>
 
-                <article className='Navbar1'>
-                <NavLink to='/'><img className='Navbar111' id='kk' src={VC} alt='' /></NavLink>
+  return (
+    <>
+      <div className='Navbar'>
 
-                    <ul className='Navbar11'>
-                        <NavLink to='/'><li className='none'>Bosh sahifa</li></NavLink>
-                        <Link to='/Tarix'><li className='none' >Tarix</li></Link>
-                        <Link to='/Diqqatga_sazovor_joylar'><li className='none'>Diqqatga sazovor joylar</li></Link>
-                        <Link to='/Faolyatlar'><li className='none' id='fa' >Faoliyatlar</li></Link>
-             
-                        <Link to='/Mahalliy_bizneslar'><li className='none'>Mahalliy bizneslar</li></Link>
+        <article className='Navbar1'>
 
-                        <Link to='/Galeriya'><li className='none'>Galereya</li></Link>
-                        <Link to='Yangiliklar'><li className='none'>Yangiliklar</li></Link>
-                        <Link to='Foydali_manbalar'><li className='none'>Foydali manbalar</li></Link>
-                        
-                        <li onClick={handleToggleCommentBox} className='none'>Sharhlar qoldirish</li>
-                    </ul>
-                    <img className='Navbar2' src={Menu} alt="" />
-                </article>
-                {showCommentBox && (
-                    <div className='comment-parent'>
-                        <div className="comment-box">
-                            <article className='vf'>
-                                <h2>Izoh qoldirish</h2>
-                                <h1 onClick={handleToggleCommentBox} className='cx'>x</h1>
-                            </article>
-                            <textarea ref={commentRef} className='area' placeholder="Izohingizni kiriting"></textarea>
-                            <button onClick={handleSendComment} className='btn'>Yuborish</button>
-                        </div>
-                    </div>
-                )}
+          <NavLink to='/'><img  className='Navbar111' id='none' src={VC} alt='' /></NavLink>
 
+          <ul className='Navbar11'>
+            <NavLink to='/'><li className='none'>Bosh sahifa</li></NavLink>
+            <Link to='/Tarix'><li className='none' >Tarix</li></Link>
+            <Link to='/Diqqatga_sazovor_joylar'><li className='none'>Diqqatga sazovor joylar</li></Link>
+            <Link to='/Faolyatlar'><li className='none' id='fa' >Faoliyatlar</li></Link>
+
+            <Link to='/Mahalliy_bizneslar'><li className='none'>Mahalliy bizneslar</li></Link>
+
+            <Link to='/Galeriya'><li className='none'>Galereya</li></Link>
+            <Link to='Yangiliklar'><li className='none'>Yangiliklar</li></Link>
+            <Link to='Foydali_manbalar'><li className='none'>Foydali manbalar</li></Link>
+
+            <li onClick={handleToggleCommentBox} className='none'>Sharhlar qoldirish</li>
+          </ul>
+          <img onClick={handleToggleCommentBoxx} className='Navbar2' src={Menu} alt="" />
+        </article>
+        {showCommentBox && (
+          <div className='comment-parent'>
+            <div className="comment-box">
+              <article className='vf'>
+                <h2>Izoh qoldirish</h2>
+                <h1 onClick={handleToggleCommentBox}  className='cx'>x</h1>
+              </article>
+              <textarea ref={commentRef} className='area' placeholder="Izohingizni kiriting"></textarea>
+              <button onClick={handleSendComment} className='btn'>Yuborish</button>
             </div>
+          </div>
+        )}
 
-        </>
+      </div>
+      <div className='popur'>
 
-    )
+        {showCommentBoxx && (
+          <div className='popup'>
+            <article className='xxx'>
+              <h1>Menyu</h1>
+              <h1 onClick={handleToggleCommentBoxx} className='xx'>x</h1>
+
+            </article>
+            <article className=''>
+              <NavLink to='/'><li className='sxa'>Bosh sahifa</li></NavLink>
+              <Link to='/Tarix'><li className='sxa' >Tarix</li></Link>
+              <Link to='/Diqqatga_sazovor_joylar'><li className='sxa'>Diqqatga sazovor joylar</li></Link>
+              <Link to='/Faolyatlar'><li className='sxa' id='' >Faoliyatlar</li></Link>
+
+              <Link to='/Mahalliy_bizneslar'><li className='sxa'>Mahalliy bizneslar</li></Link>
+
+              <Link to='/Galeriya'><li className='sxa'>Galereya</li></Link>
+              <Link to='Yangiliklar'><li className='sxa'>Yangiliklar</li></Link>
+              <Link to='Foydali_manbalar'><li className='sxa'>Foydali manbalar</li></Link>
+
+              <li onClick={handleToggleCommentBox} className='sxa'>Sharhlar qoldirish</li>
+            </article>
+          </div>
+        )
+
+        }
+
+      </div>
+    </>
+
+  )
 }
 
 export default Navbar
